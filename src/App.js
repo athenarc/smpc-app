bcee23ba72249dbfef5a1901ffdde7f8c3c8dff7
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-11 19:16:44 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import $ from 'jquery';
 import Popper from 'popper.js';
@@ -9,12 +10,25 @@ import Header from './header/Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/scss/main.scss'
 
+import appRoutes from './routes/index.js'
+
+const switchRoutes = (
+  <Switch>
+    {appRoutes.map((prop, key) => {
+      return <Route path={prop.path} component={prop.component} key={key} exact />
+    })}
+  </Switch>
+)
+
 class App extends Component {
   render() {
     return (
-      <main className="App">
+      <div>
         <Header />
-      </main>
+        <main className="App">
+          {switchRoutes}
+        </main>
+      </div>
     );
   }
 }
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import Home from '../home/Home'
+
+const appRoutes = [
+  {path: '/', component: Home}
+]
+
+export default appRoutes
