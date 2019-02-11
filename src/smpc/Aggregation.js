22016bc7a39094ce173822d87f4d0b306613dc61
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-11 20:14:48 +0200

    Create smpc pages

---
+++ src/smpc/Aggregation.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+class Aggregation extends React.Component {
+  render() {
+    return (
+      <section className='home'>
+        <div className='jumbotron'>
+          <h1 className='display-4'>Secure Data Aggregation</h1>
+          <p className='lead'>Perform analytics using privacy-preserving data aggregation algorithms for computing and visualizing multi-dimemsional histograms.</p>
+          <hr className='my-4' />
+          <p>
+          MeSH Dataset: MeSH provides a hierarchically-organized terminology for
+          indexing and cataloging of biomedical information such as
+          MEDLINE/PUBmed and other United States National Library of Medicine
+          (NLM) databases. Created and updated by the NLM, it is used by
+          articles databases and by NLM's catalog of book holdings. This dataset
+          is based on the MeSH tree structure. MeSH terms are represented as
+          normalized values; this means that even attributes like Age, are
+          separated into groups (for instance Child, Adult, etc). This dataset
+          contains semantically annotated patient data.
+          </p>
+          <p>
+          CVI Dataset: Cardiovascular disease is a class of diseases that
+          involve the heart or blood vessels. Cardiovascular disease includes
+          coronary artery diseases (CAD) such as angina and myocardial
+          infarction (commonly known as a heart attack). This dataset contains
+          CardioVascular Imaging (CVI) information, which are represented as
+          numerical values – not normalized.
+          </p>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Aggregation
---
+++ src/smpc/Classification.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+class Classification extends React.Component {
+  render() {
+    return (
+      <section className='home'>
+        <div className='jumbotron'>
+          <h1 className='display-4'>Secure Data Classification</h1>
+          <p className='lead'>Generate decision trees from patient data using the privacy-preserving ID3 classifier.</p>
+          <hr className='my-4' />
+          <p> MeSH Dataset: MeSH provides a hierarchically-organized terminology
+          for indexing and cataloging of biomedical information such as
+          MEDLINE/PUBmed and other United States National Library of Medicine
+          (NLM) databases. Created and updated by the NLM, it is used by
+          articles databases and by NLM's catalog of book holdings. This dataset
+          is based on the MeSH tree structure. MeSH terms are represented as
+          normalized values; this means that even attributes like Age, are
+          separated into groups (for instance Child, Adult, etc). </p>
+          <p>
+          CVI Dataset: Cardiovascular disease is a class of diseases that
+          involve the heart or blood vessels. Cardiovascular disease includes
+          coronary artery diseases (CAD) such as angina and myocardial
+          infarction (commonly known as a heart attack). This dataset contains
+          CardioVascular Imaging (CVI) information, which are represented as
+          numerical values – not normalized.
+          </p>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Classification
