f6951c36e0a8918af5d99ade3fffa0dfa2007e0f
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-12 22:04:06 +0200

    Fix store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,27 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 import throttle from 'lodash/throttle'
+
 import { loadState, saveState } from './localStorage'
 import appReducers from './reducers'
+
+const loggerMiddleware = createLogger()
 const persistedState = loadState()
+
+const store = createStore(
   appReducers,
   persistedState,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+)
+
 store.subscribe(throttle(() => {
   saveState({
     requests: store.getState().requests
   })
 }), 1000)
+
+export default store
