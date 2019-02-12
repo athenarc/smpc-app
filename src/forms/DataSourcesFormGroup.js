ad9fa853f4d7f65cd252206c1d0c0113fb857562
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-12 22:05:06 +0200

    Create datasource form fields

---
+++ src/forms/DataSourcesFormGroup.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Field } from 'react-final-form'
+
+const DataSourceFormGroup = ({ ...props }) => {
+  return (  
+    <div className='form-group form-check form-check-inline datasources'>
+      <div className='custom-control custom-switch datasource'>
+        <Field type='checkbox' value='data-provider-1' name='data-providers' id='data-provider-1' component='input' className='custom-control-input' />
+        <label className='custom-control-label' htmlFor='data-provider-1'>Hospital A</label>
+      </div>
+      <div className='custom-control custom-switch datasource'>
+        <Field type='checkbox' value='data-provider-2' name='data-providers' id='data-provider-2' component='input' className='custom-control-input' />
+        <label className='custom-control-label' htmlFor='data-provider-2'>Hospital B</label>
+      </div>
+    </div>
+  )
+}
+
+export default DataSourceFormGroup
