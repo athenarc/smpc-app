d33786faf59b05a0cce0d0629752e2d981d8a48b
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-13 16:50:20 +0200

    Seperate form logic

--- src/forms/HistogramForm.js
+++ src/forms/HistogramForm.js
@@ -1,34 +1,14 @@
 import React from 'react'
 import { Form } from 'react-final-form'
 
-import DataSourceFormGroup from './DataSourcesFormGroup'
-import FiltersFormGroup from '../forms/FiltersFormGroup'
-import AttributesFormGroup from '../forms/AttributesFormGroup'
-import withRepeatableRow from '../forms/RepeatableRow'
+import HistogramFormComponent from './HistogramFormComponent'
+import withForm from './FormHOC'
 
-const AttributeRepeatableRow = withRepeatableRow(AttributesFormGroup)
-const FilterRepeatableRow = withRepeatableRow(FiltersFormGroup)
+const FormComponent = withForm(HistogramFormComponent)
 
 class HistogramForm extends React.Component {
   render () {
-    return <Form
-      onSubmit={this.props.onSubmit}
-      render={({ handleSubmit, form, submitting, values }) => (
-        <form onSubmit={handleSubmit}>
-          <h4 class='mb-3'>Attributes</h4>
-          <AttributeRepeatableRow />
-          <hr class='mb-4' />
-          <h4 class='mb-3'>Filters</h4>
-          <FilterRepeatableRow />
-          <hr class='mb-4' />
-          <h4 class='mb-3'>Datasources</h4>
-          <hr class='mb-4' />
-          <DataSourceFormGroup />
-          <hr class='mb-4' />
-          <button className='btn btn-primary btn-lg btn-block' type='submit' disabled={submitting}>Request computation</button>
-        </form>
-        )}
-      />
+    return <FormComponent />
   }
 }
 
---
+++ src/forms/HistogramFormComponent.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Form } from 'react-final-form'
+import arrayMutators from 'final-form-arrays'
+
+import DataSourceFormGroup from './DataSourcesFormGroup'
+import FiltersFormGroup from './FiltersFormGroup'
+import AttributesFormGroup from './AttributesFormGroup'
+import withRepeatableRow from './RepeatableRow'
+
+const AttributeRepeatableRow = withRepeatableRow(AttributesFormGroup, 'attributes')
+const FilterRepeatableRow = withRepeatableRow(FiltersFormGroup, 'filters')
+
+class HistogramForm extends React.Component {
+  render () {
+    return <Form
+      onSubmit={this.props.onSubmit}
+      mutators={{
+        ...arrayMutators
+      }}
+      render={({
+        handleSubmit,
+        form: { mutators: { push, pop } },
+        pristine,
+        reset,
+        submitting,
+        values
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <h4 className='mb-3'>Attributes</h4>
+          <AttributeRepeatableRow push={push}/>
+          <hr className='mb-4' />
+          <h4 className='mb-3'>Filters</h4>
+          <FilterRepeatableRow push={push} />
+          <hr className='mb-4' />
+          <h4 className='mb-3'>Datasources</h4>
+          <hr className='mb-4' />
+          <DataSourceFormGroup />
+          <hr className='mb-4' />
+          <button className='btn btn-primary btn-lg btn-block' type='submit' disabled={submitting}>Request computation</button>
+        </form>
+        )}
+      />
+  }
+}
+
+export default HistogramForm
