904e19cdd05a60808d129cd3843051e6fcf28cf0
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-13 16:49:59 +0200

    Dynamic names

--- src/forms/AttributesFormGroup.js
+++ src/forms/AttributesFormGroup.js
@@ -5,14 +5,15 @@ const AttributesFormGroup = ({ ...props }) => {
   return (
     <div className='form-row filters'>
       <div className='form-group col-md-6'>
-        <label htmlFor='attributes'>Attribute</label>
-        <Field id='attributes' name='attributes' component='select' className='custom-select'>
+        <label htmlFor={`${props.name}.attribute`}>Attribute</label>
+        <Field id={`${props.name}.attribute`} name={`${props.name}.attribute`} component='select' className='custom-select'>
+          <option />
           <option value='age'>Age</option>
         </Field>
       </div>
       <div className='form-group col-md-6'>
-        <label htmlFor='filter-value'>Cells</label>
-        <Field id='cells' name='cells' component='input' className='form-control' type='number' placeholder='Number of cells' />
+        <label htmlFor={`${props.name}.cells`}>Cells</label>
+        <Field id={`${props.name}.cells`} name={`${props.name}.cells`} component='input' className='form-control' type='number' placeholder='Number of cells' />
       </div>
     </div>
   )
--- src/forms/FiltersFormGroup.js
+++ src/forms/FiltersFormGroup.js
@@ -5,22 +5,23 @@ const FilterFormGroup = ({ ...props }) => {
   return (
     <div className='form-row filters'>
       <div className='form-group col-md-4'>
-        <label htmlFor='attributes'>Attribute</label>
-        <Field id='attributes' name='attributes' component='select' className='custom-select'>
+        <label htmlFor={`${props.name}.attribute`}>Attribute</label>
+        <Field id={`${props.name}.attribute`} name={`${props.name}.attribute`} component='select' className='custom-select'>
+          <option />
           <option value='age'>Age</option>
         </Field>
       </div>
       <div className='form-group col-md-4'>
-        <label htmlFor='operators'>Operator</label>
-        <Field id='operators' name='operators' component='select' className='custom-select'>
+        <label htmlFor={`${props.name}.operator`}>Operator</label>
+        <Field id={`${props.name}.operator`} name={`${props.name}.operator`} component='select' className='custom-select'>
           <option value='='>=</option>
           <option value='<'>&lsaquo;</option>
           <option value='>'>&rsaquo;</option>
         </Field>
       </div>
       <div className='form-group col-md-4'>
-        <label htmlFor='filter-value'>Filter Value</label>
-        <Field id='filter-value' name='filter-value' component='input' className='form-control' type='text' placeholder='Filter value' />
+        <label htmlFor={`${props.name}.value`}>Filter Value</label>
+        <Field id={`${props.name}.value`} name={`${props.name}.value`} component='input' className='form-control' type='text' placeholder='Filter value' />
       </div>
     </div>
   )
