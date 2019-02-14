d59704b12a8019e51df86dba814ae2dda5778796
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-14 13:20:55 +0200

    Check status reducer

--- src/reducers/computations.js
+++ src/reducers/computations.js
@@ -1,9 +1,32 @@
 import types from '../actions/actionTypes'
 import { createReducer } from '../helpers'
 
+const findComputationById = (arr, id) => {
+  return arr.findIndex((item) => item.id === id)
+}
+
+const includesComputation = (arr, computation) => {
+    return arr.map(item => item.id).includes(computation.id)
+}
+
 const computations = createReducer([], {
   [types.ADD_COMPUTATION]: (state, action) => { return [ ...state ] },
   [types.SUBMIT_COMPUTATION_SUCCESS]: (state, action) => { return [ ...state, { ...action.payload.data } ] },
+  [types.CHECK_COMPUTATION_STATUS_SUCCESS]: (state, action) => {
+
+    let computationIndex = findComputationById(state, action.payload.data.id)
+
+    if (computationIndex !== -1) {
+      const comp = { ...state[computationIndex], ...action.payload.data }
+      return state
+        .slice(0, computationIndex)
+        .concat([comp])
+        .concat(state.slice(computationIndex + 1))
+    }
+
+    return [ ...state, { ...action.payload.data } ]
+  }
+
 })
 
 export default computations
