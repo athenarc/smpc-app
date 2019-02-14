2df203d28c8ab1261e8307a8bd56638f317669fc
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-14 13:20:50 +0200

    Submit computation  reducer

--- src/reducers/computations.js
+++ src/reducers/computations.js
@@ -3,6 +3,7 @@ import { createReducer } from '../helpers'
 
 const computations = createReducer([], {
   [types.ADD_COMPUTATION]: (state, action) => { return [ ...state ] },
+  [types.SUBMIT_COMPUTATION_SUCCESS]: (state, action) => { return [ ...state, { ...action.payload.data } ] },
 })
 
 export default computations
