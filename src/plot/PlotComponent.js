a8a755617a706efa2f452e6c40c1848b673125df
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-11 22:08:20 +0300

    Refactor plot component

--- src/plot/PlotComponent.js
+++ src/plot/PlotComponent.js
@@ -1,41 +1,19 @@
 import React from 'react'
 import Plot from 'react-plotly.js'
 
-class PlotComponent extends React.Component {
-    constructor(props) {
-        super(props)
-        const trace = {
-          type: 'bar',
-          x: ['giraffes', 'orangutans', 'monkeys'],
-          y: [20, 14, 23]
-        }
-
-        const layout = {
-          paper_bgcolor: '#f8f9fa',
-          plot_bgcolor: '#f8f9fa'
-        }
-
-        const config = {
-          responsive: true
-        }
-
-        this.state = { data: [trace], layout, frames: [], config };
-    }
-
-    render() {
-        return (
-            <Plot
-                data={this.state.data}
-                layout={this.state.layout}
-                frames={this.state.frames}
-                config={this.state.config}
-                onInitialized={(figure) => this.setState(figure)}
-                onUpdate={(figure) => this.setState(figure)}
-                useResizeHandler={true}
-                style={{width: '100%', height: '100%' }}
+const PlotComponent = (props) => {
+    return (
+      <Plot
+        data={props.data}
+        layout={props.layout}
+        frames={props.frames}
+        config={props.config}
+        onInitialized={(figure) => props.onInitialized(figure)}
+        onUpdate={(figure) => props.onUpdate(figure)}
+        useResizeHandler
+        style={{width: '100%', height: '100%' }}
             />
-        );
-    }
-}
+    )
+  }
 
 export default PlotComponent
