3dc1b7815436cc67703f822e6329d524e89286e4
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-12 17:51:14 +0300

    Remove log

--- src/reducers/notifications.js
+++ src/reducers/notifications.js
@@ -3,7 +3,7 @@ import { createReducer } from '../helpers'
 
 const notifications = createReducer([], {
   [types.ADD_NOTIFICATION]: (state, action) => { return [ ...state, { ...action.payload.data } ] },
-  [types.REMOVE_NOTIFICATION]: (state, action) => { console.log(action); return state.filter(toast => toast.id !== action.payload.data.id) }
+  [types.REMOVE_NOTIFICATION]: (state, action) => { return state.filter(toast => toast.id !== action.payload.data.id) }
 })
 
 export default notifications
