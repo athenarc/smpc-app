f7ab8ad16fd4aa7b31fff778cd2832b2f612efe6
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-12 17:50:58 +0300

    Add update computation reducer

--- src/reducers/computations.js
+++ src/reducers/computations.js
@@ -1,6 +1,15 @@
 import types from '../actions/actionTypes'
 import { createReducer, findComputationIndexById } from '../helpers'
 
+const submitComputation = (state, action) => {
+  let computationIndex = findComputationIndexById(state, action.payload.data.id) // websocket already added
+  if (computationIndex !== -1) {
+    return [ ...state ]
+  }
+
+  return updateComputations(state, action)
+}
+
 const updateComputations = (state, action) => {
   let computationIndex = findComputationIndexById(state, action.payload.data.id)
 
@@ -17,6 +26,7 @@ const updateComputations = (state, action) => {
 
 const computations = createReducer([], {
   [types.ADD_COMPUTATION]: (state, action) => { return [ ...state, ...action.payload.data ] },
+  [types.UPDATE_COMPUTATION]: (state, action) => { return updateComputations(state, action) },
   [types.DELETE_COMPUTATION]: (state, action) => {
     let computationIndex = findComputationIndexById(state, action.payload.data.id)
 
@@ -28,7 +38,7 @@ const computations = createReducer([], {
 
     return [ ...state ]
   },
-  [types.SUBMIT_COMPUTATION_SUCCESS]: (state, action) => { return [ ...state, { ...action.payload.data } ] },
+  [types.SUBMIT_COMPUTATION_SUCCESS]: (state, action) => { return submitComputation(state, action) },
   [types.CHECK_COMPUTATION_STATUS_SUCCESS]: (state, action) => { return updateComputations(state, action) },
   [types.GET_SINGLE_COMPUTATION_SUCCESS]: (state, action) => { return updateComputations(state, action) }
 
