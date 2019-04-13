cbad5728aaab6a69a88c0178301a09fbb6a52c50
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-13 21:51:01 +0300

    Implement ping pong

--- src/actions/websocket.js
+++ src/actions/websocket.js
@@ -1,12 +1,28 @@
 import types from './actionTypes'
 
 const URI = process.env.REACT_APP_WS || 'ws://localhost:3000'
-
 const webSocket = new WebSocket(URI);
+
+let pingTimeout
+
+const heartbeat = () => {
+  clearTimeout(pingTimeout);
+
+  pingTimeout = setTimeout(() => {
+    webSocket.close();
+  }, 30000 + 1000) // 30 seconds
+}
 
 const initWebsocket = (store) => {
     webSocket.onmessage = (event) => {
       const data = JSON.parse(event.data)
+
+      if (data.message && data.message === 'ping') {
+        webSocket.send(JSON.stringify({ message: 'pong' }))
+        heartbeat()
+        return
+      }
+
       store.dispatch({ type: types.UPDATE_COMPUTATION, payload: { data: data.job } })
       let msg = {}
 
@@ -27,8 +43,14 @@ const initWebsocket = (store) => {
       store.dispatch({ type: types.ADD_NOTIFICATION, payload: { data: msg } })
     }
 
-    webSocket.onopen = () => console.log(` Ws: Connected to ${URI}`)
-    webSocket.onclose = () => console.log(` Ws: Disconnected from server`)
+    webSocket.onopen = () => {
+      console.log(` Ws: Connected to ${URI}`)
+      heartbeat()
+    }
+    webSocket.onclose = () =>  {
+      console.log(` Ws: Disconnected from server`)
+      clearTimeout(pingTimeout)
+    }
 }
 
 const emit = (type, payload) => webSocket.send(JSON.stringify({ type, payload }))
