c16e2aad53d95602514c3268ce82078cac6f6db7
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-16 19:59:09 +0300

    Delete mesh text

--- src/smpc/Histogram.js
+++ src/smpc/Histogram.js
@@ -11,17 +11,6 @@ class Histogram extends React.Component {
             <p className='lead'>Perform analytics using privacy-preserving data aggregation algorithms for computing and visualizing multi-dimemsional histograms.</p>
             <hr className='my-4' />
             <p>
-            MeSH Dataset: MeSH provides a hierarchically-organized terminology for
-            indexing and cataloging of biomedical information such as
-            MEDLINE/PUBmed and other United States National Library of Medicine
-            (NLM) databases. Created and updated by the NLM, it is used by
-            articles databases and by NLM's catalog of book holdings. This dataset
-            is based on the MeSH tree structure. MeSH terms are represented as
-            normalized values; this means that even attributes like Age, are
-            separated into groups (for instance Child, Adult, etc). This dataset
-            contains semantically annotated patient data.
-            </p>
-            <p>
             CVI Dataset: Cardiovascular disease is a class of diseases that
             involve the heart or blood vessels. Cardiovascular disease includes
             coronary artery diseases (CAD) such as angina and myocardial
