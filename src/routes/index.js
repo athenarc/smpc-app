e1403536735c099a8e37036e6173c6cf326aa6f1
Christos Nasikas <xristosnasikas@gmail.com>
2019-05-31 16:22:27 +0300

    Create login route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,15 @@ import Histogram from '../smpc/Histogram'
 import Dashboard from '../computation/Dashboard'
 import Computations from '../computation/Computations'
 import SingleComputation from '../computation/SingleComputation'
+import Login from '../authentication/Login'
 
 const appRoutes = [
   { path: '/', component: Home },
   { path: '/histogram', component: Histogram },
   { path: '/dashboard', component: Dashboard },
   { path: '/computations', component: Computations },
-  { path: '/computation/:id', component: SingleComputation }
+  { path: '/computation/:id', component: SingleComputation },
+  { path: '/login', component: Login }
 ]
 
 export default appRoutes
