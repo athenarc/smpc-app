bfcca3473dec90a9066a6617d7eb09a0cae48123
Christos Nasikas <xristosnasikas@gmail.com>
2019-06-18 16:40:02 +0300

    Refactor histogram form

--- src/forms/HistogramForm.js
+++ src/forms/HistogramForm.js
@@ -1,10 +1,95 @@
 import React from 'react'
 
-import HistogramFormComponent from './HistogramFormComponent'
+import { Form } from 'react-final-form'
+import arrayMutators from 'final-form-arrays'
+
+import Algorithms from './Algorithms'
+import DataSourceFormGroup from './DataSourcesFormGroup'
+import FiltersFormGroup from './FiltersFormGroup'
+import AttributesFormGroup from './AttributesFormGroup'
+import withRepeatableRow from './RepeatableRow'
+
 import withForm from './FormHOC'
 
-const FormComponent = withForm(HistogramFormComponent, { action: 'requestHistogram' } )
+import keywords from '../smpc-global/keywords.json'
+import meshAttributes from '../smpc-global/meshAttributes.json'
+import algorithms from '../smpc-global/algorithms.json'
 
-const HistogramForm = () => <FormComponent />
+const AttributeRepeatableRow = withRepeatableRow(AttributesFormGroup, 'attributes')
+const FilterRepeatableRow = withRepeatableRow(FiltersFormGroup, 'filters')
 
-export default HistogramForm
+class HistogramForm extends React.Component {
+  constructor () {
+    super()
+    this.listeners = {
+      algorithm: this.onAlgorithmChange.bind(this)
+    }
+
+    this.state = {
+      attributes: keywords,
+      algorithms: algorithms.filter(alg => alg.name.includes('histogram'))
+    }
+  }
+
+  onAlgorithmChange (event) {
+    const algorithm = event.target.value
+
+    switch (algorithm) {
+      case '1d_categorical_histogram':
+      case '2d_categorical_histogram':
+        this.setState({
+          attributes: keywords
+        })
+        break
+      case '1d_numerical_histogram':
+      case '2d_numerical_histogram':
+        this.setState({
+          attributes: meshAttributes
+        })
+        break
+      case '2d_mixed_histogram':
+        this.setState({
+          attributes: [ ...meshAttributes, ...keywords ]
+        })
+        break
+      default:
+        break
+    }
+  }
+
+  render () {
+    return (
+      <Form
+        onSubmit={this.props.onSubmit}
+        mutators={{
+          ...arrayMutators
+        }}
+        render={({
+        handleSubmit,
+        form: { mutators: { push, pop } },
+        pristine,
+        reset,
+        submitting,
+        values
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <h4 className='mb-3'>Algorithm</h4>
+          <Algorithms algorithms={algorithms} listener={this.listeners.algorithm} />
+          <h4 className='mb-3'>Attributes</h4>
+          <AttributeRepeatableRow push={push} attributes={this.state.attributes} />
+          <hr className='mb-4' />
+          <h4 className='mb-3'>Filters</h4>
+          <FilterRepeatableRow push={push} attributes={this.state.attributes} />
+          <hr className='mb-4' />
+          <h4 className='mb-3'>Datasources</h4>
+          <hr className='mb-4' />
+          <DataSourceFormGroup />
+          <hr className='mb-4' />
+          <button className='btn btn-primary btn-lg btn-block' type='submit' disabled={submitting}>Request computation</button>
+        </form>
+        )}
+      />)
+  }
+}
+
+export default withForm(HistogramForm, { action: 'requestHistogram' })
