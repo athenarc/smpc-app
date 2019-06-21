2afbf988195ebf5246049df5e93fea40a305cceb
Christos Nasikas <xristosnasikas@gmail.com>
2019-06-21 19:18:53 +0300

    Fix content

--- src/smpc/Histogram.js
+++ src/smpc/Histogram.js
@@ -11,20 +11,11 @@ class Histogram extends React.Component {
             <p className='lead'>Perform analytics using privacy-preserving data aggregation algorithms for computing and visualizing multi-dimemsional histograms.</p>
             <hr className='my-4' />
             <p>
-              Medical Subject Headings (MeSH) provides a hierarchically-organized terminology for indexing
-              and cataloging of biomedical information such as MEDLINE/PUBmed and
-              other United States National Library of Medicine (NLM) databases.
-              Created and updated by the NLM, it is used by articles databases and
-              by NLM's catalog of book holdings. This dataset is based on the MeSH
-              tree structure. MeSH terms are represented as normalized values;
-              this means that even attributes like <i>Age</i>, are separated into
-              groups (for instance <i>Child</i>, <i>Adult</i>, etc). This dataset
-              contains semantically annotated patient data.
-              In order to request an aggregation, the attributes must be specified and
-              should be in the form that is defined by MeSH. For instance, for attribute
-              <i>Age</i>, provide the <i>M01.060</i> MeSH ID, which has the following
-              subcategories: <i>    Adolescent [M01.060.057], Adult [M01.060.116], Child
-              [M01.060.406], Infant [M01.060.703]</i>.</p>
+              Medical Subject Headings (MeSH) provides a hierarchically-organized terminology for indexing and cataloguing biomedical information such as MEDLINE/PUBmed and other databases from the US National Library of Medicine (NLM). Created and updated by the NLM, MeSH is used in articles, databases and the NLM's catalog of book holdings. MeSH terms are represented as normalized values. That is, even numerical attributes such as "Age", are separated into groups (for instance Child, Adult, etc).
+            </p>
+            <p>
+              This dataset contains semantically annotated patient data based on the MeSH tree structure. In order to request an aggregation, the attributes must be specified and should be in the form that is defined by MeSH. For instance, for the attribute "Age", one would need to provide the MeSH ID M01.060, which has the following subcategories: Adolescent [M01.060.057], Adult [M01.060.116], Child [M01.060.406] and Infant [M01.060.703].
+              </p>
               <p>Search mesh terms at: <a href='https://meshb.nlm.nih.gov/treeView' role='button' target='_blank' rel='noopener noreferrer'>MeSH Browser Tree View</a></p>
           </div>
         </div>
