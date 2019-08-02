e4a34ac1175c42dbeeb0d20df03e57849894aee2
Christos Nasikas <xristosnasikas@gmail.com>
2019-08-02 20:01:06 +0300

    Create draft authentication service

---
+++ src/services/authentication.js
@@ -0,0 +1,16 @@
+class AuthenticationService {
+  init () {}
+  isLoggedIn () {}
+  setToken(token) {
+    
+  }
+  getToken() {}
+  loggin () {}
+  logout () {}
+
+}
+
+// Sigleton Service
+const auth = new AuthenticationService()
+
+export default auth
