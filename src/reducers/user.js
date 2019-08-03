2646e22afd932b4ad201df17914cd9a5fa41c444
Christos Nasikas <xristosnasikas@gmail.com>
2019-08-03 21:27:04 +0300

    Add reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -2,8 +2,9 @@ import types from '../actions/actionTypes'
 import { createReducer } from '../helpers'
 
 const user = createReducer({}, {
-  [types.LOGIN_SUCCESS]: (state, action) => ({ ...state, ...action.payload.data }),
-  [types.LOGOUT]: (state, action) => ({ user: {} })
+  [types.LOGIN_SUCCESS]: (state, action) => ({ ...state, ...action.payload.data, isAuthenticated: true }),
+  [types.LOGOUT]: (state, action) => ({ user: { isAuthenticated: false } }),
+  [types.UNAUTHORIZED]: (state, action) => ({ user: { isAuthenticated: false } })
 })
 
 export default user
