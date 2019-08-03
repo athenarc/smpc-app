c4d1b306607350edf062c2b899c74b0d2d64d308
Christos Nasikas <xristosnasikas@gmail.com>
2019-08-03 21:26:05 +0300

    Enable private routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 import Header from './header/Header'
 import NotificationService from './notification/NotificationService'
+import PrivateRoute from './routes/PrivateRoute'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/scss/main.scss'
 
 import appRoutes from './routes/index.js'
 
-const switchRoutes = (
-  <Switch>
-    {appRoutes.map((prop, key) => {
-      return <Route path={prop.path} component={prop.component} key={key} exact />
-    })}
-  </Switch>
-)
-
 class App extends Component {
   render () {
+    const { user } = this.props
+    const isAuthenticated = user && user.isAuthenticated
     return (
       <div>
         <Header />
         <NotificationService />
         <main className='main' role='main'>
-          {switchRoutes}
+          <Switch>
+            {appRoutes.map((prop, key) => {
+              return prop.private
+              ? <PrivateRoute path={prop.path} component={prop.component} key={key} exact isAuthenticated={isAuthenticated} />
+              : <Route path={prop.path} component={prop.component} key={key} exact />
+            })}
+          </Switch>
         </main>
       </div>
     )
   }
 }
 
-export default App
+const mapStateToProps = (state, ownProps) => ({ user: state.user })
+
+export default withRouter(connect(mapStateToProps, null)(App))
