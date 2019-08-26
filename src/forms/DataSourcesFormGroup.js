dc0e65b4429cdd0ff97b718bd6a84c4318e29313
Christos Nasikas <xristosnasikas@gmail.com>
2019-08-26 18:56:22 +0300

    Fix data source ids

--- src/forms/DataSourcesFormGroup.js
+++ src/forms/DataSourcesFormGroup.js
@@ -5,16 +5,16 @@ const DataSourceFormGroup = () => {
   return (
     <div className='form-group form-check form-check-inline datasources'>
       <div className='custom-control custom-switch datasource'>
+        <Field type='checkbox' value='0' name='dataProviders' id='0' component='input' className='custom-control-input' />
+        <label className='custom-control-label' htmlFor='0'>Hospital A</label>
+      </div>
+      <div className='custom-control custom-switch datasource'>
         <Field type='checkbox' value='1' name='dataProviders' id='1' component='input' className='custom-control-input' />
-        <label className='custom-control-label' htmlFor='1'>Hospital A</label>
+        <label className='custom-control-label' htmlFor='1'>Hospital B</label>
       </div>
       <div className='custom-control custom-switch datasource'>
         <Field type='checkbox' value='2' name='dataProviders' id='2' component='input' className='custom-control-input' />
-        <label className='custom-control-label' htmlFor='2'>Hospital B</label>
-      </div>
-      <div className='custom-control custom-switch datasource'>
-        <Field type='checkbox' value='3' name='dataProviders' id='3' component='input' className='custom-control-input' />
-        <label className='custom-control-label' htmlFor='3'>Hospital C</label>
+        <label className='custom-control-label' htmlFor='2'>Hospital C</label>
       </div>
     </div>
   )
